30e4e49b649340236bc6f82e3df321cf023cdf4c
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-14 15:30:44 +0100

    router setup

---
+++ src/Layout/Main.js
@@ -0,0 +1,14 @@
+import React from 'react';
+import Footer from '../Components/Shared/Footer';
+import Header from '../Components/Shared/Header';
+
+const Main = () => {
+    return (
+        <div>
+            <Header/>
+            <Footer/>
+        </div>
+    );
+};
+
+export default Main;
