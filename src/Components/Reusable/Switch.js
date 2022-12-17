34108661269da67a350d60af78447de67fc8d4d9
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-17 21:36:19 +0100

    dynamic switch

--- src/Components/Reusable/Switch.js
+++ src/Components/Reusable/Switch.js
@@ -1,24 +1,36 @@
-import React, { useState } from 'react';
+import React, { useLayoutEffect, useRef, useState } from 'react';
 
 const Switch = ({items}) => {
     const [isToggled, setToggle]= useState(false);
+
+    const [item1Width , setItem1Width]= useState();
+    const [item2Width , setItem2Width]= useState();
+    const item1Ref =useRef();
+    const item2Ref = useRef();
+
+    useLayoutEffect(()=>{
+        setItem1Width(item1Ref.current.offsetWidth)
+        setItem2Width(item2Ref.current.offsetWidth)
+    },[])
 
     const activeText = 'text-black'
 
 
     return (
         <div className='flex items-center h-8 border-solid border-blue-900 rounded-3xl text-white font-semibold border-2 relative z-[1]'>
-            <div onClick={()=>setToggle(false)} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled || activeText}`}>{items[0]}</div>
-            <div  onClick={()=>setToggle(true)} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled && activeText}`}>{items[1]}</div>
+            <div ref={item1Ref} onClick={()=>setToggle(false)} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled || activeText}`}>{items[0]}</div>
+
+            <div ref={item2Ref} onClick={()=>setToggle(true)} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled && activeText}`}>{items[1]}</div>
+
             <div className='h-8 w-24 rounded-3xl bg-yellow-400 absolute z-[-1] transition-all duration-200 ease-in' style={
                 isToggled ?{
-                    left: '80px',
-                    width: '120px'
+                    left: `${item1Width+2}px`,
+                    width: `${item2Width+2}px`
                 }
                 :
                 {
-                    left: '0px',
-                    width: '80px'
+                    left: '-1px',
+                    width: `${item1Width}px`
                 }
             }></div>
         </div>
