602670953502cabddac4107fb27c0da25527a546
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-17 23:35:21 +0100

    movies

--- src/Components/Reusable/MovieList.js
+++ src/Components/Reusable/MovieList.js
@@ -1,70 +1,21 @@
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import MovieCard from './MovieCard';
 
 const MovieList = () => {
-    const movies= [
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        {
-            title: 'Wednesday',
-            released: '2022',
-            imageUrl: 'https://image.tmdb.org/t/p/w220_and_h330_face/9PFonBhy4cQy7Jz20NpMygczOkv.jpg'
-        },
-        
-    ]
+
+    const { movies } = useQuery({
+        queryKey: ['movies'],
+        queryFn: () =>
+          fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=6d47a4eb4a550f0aec87d70e03ce12ae').then(res =>
+            res.json()
+          )
+      })
     return (
         <div className='flex gap-7 overflow-x-auto'>
         {
-            movies?.map((movie, i)=>
-                <MovieCard key={i} title={movie.title} released={movie.released} imageUrl={movie.imageUrl} />
+            movies?.results?.map((movie, i)=>
+                <MovieCard key={i} title={movie.title} released={movie.released} imageUrl={movie.poster_path} />
             )
         }
             
