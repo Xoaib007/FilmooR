2f83c60ea16458ec8b24b97ef4427639e46573b4
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-17 23:50:39 +0100

    movies

--- src/Components/Reusable/MovieCard.js
+++ src/Components/Reusable/MovieCard.js
@@ -1,14 +1,19 @@
 import React from 'react';
 
-const MovieCard = ({imageUrl , title, released, rating}) => {
+const MovieCard = ({imageUrl , title, rating, voteCount}) => {
     return (
-        <div className='mb-20 '>
+        <div className=''>
+        <div className='mb-20 text-white'>
             <img src={`https://image.tmdb.org/t/p/w220_and_h330_face/${imageUrl}`} alt=''/>
             <div>
-                <p className='text-white text-left text-xl font-bold'>{title}</p>
-                <p className='text-white text-left text-xl'>{released}</p>
-                <p>{rating}</p>
+                <p className=' text-left text-xl font-bold'>{title}</p>
+                <div>
+                    <p>{rating}</p>
+                    <p>{voteCount}</p>
+                </div>
+                
             </div>
+        </div>
         </div>
     );
 };
--- src/Components/Reusable/MovieList.js
+++ src/Components/Reusable/MovieList.js
@@ -7,18 +7,24 @@ const MovieList = () => {
     const { data: movies = [] } = useQuery({
         queryKey: ['movies'],
         queryFn: () =>
-          fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=6d47a4eb4a550f0aec87d70e03ce12ae').then(res =>
-            res.json()
-          )
-      })
+            fetch('https://api.themoviedb.org/3/trending/movie/day?api_key=6d47a4eb4a550f0aec87d70e03ce12ae').then(res =>
+                res.json()
+            )
+    })
     return (
         <div className='flex gap-7 overflow-x-auto'>
-        {
-            movies?.results?.map((movie, i)=>
-                <MovieCard key={i} title={movie.title} released={movie.released} imageUrl={movie.poster_path} />
-            )
-        }
-            
+            {
+                movies?.results?.map((movie, i) =>
+                    <MovieCard
+                        key={i}
+                        title={movie.title}
+                        imageUrl={movie.poster_path}
+                        rating={movie.vote_average}
+                        voteCount={movie.vote_count}
+                    />
+                )
+            }
+
         </div>
     );
 };
