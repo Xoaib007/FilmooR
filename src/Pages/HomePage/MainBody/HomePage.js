7f1331b6d917be091d09542194c70a836e8eca71
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-18 23:54:40 +0100

    movies

--- src/Pages/HomePage/MainBody/HomePage.js
+++ src/Pages/HomePage/MainBody/HomePage.js
@@ -18,7 +18,7 @@ const HomePage = () => {
 
     return (
         <div className='min-h-screen px-32'>
-            <Section title='Whats New' items={['On TV', 'in theatre']} onToggle={handleToggle.bind(null, 'first')} isToggled={selected.first==='On TV'?false:true}>
+            <Section title='Whats New' items={['On TV', 'In Theatre']} onToggle={handleToggle.bind(null, 'first')} isToggled={selected.first==='On TV'?false:true}>
                 <MovieList fetch={selected.first}/>
             </Section>
         </div>
