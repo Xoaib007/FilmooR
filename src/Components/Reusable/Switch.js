ada3e3d6572d7b647eb1a17aa4a3b28228509b67
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-18 22:37:19 +0100

    movies

--- src/Components/Reusable/Switch.js
+++ src/Components/Reusable/Switch.js
@@ -22,7 +22,7 @@ const Switch = ({items, onToggle, isToggled}) => {
         <div className='flex items-center h-8 border-solid border-blue-900 rounded-3xl text-white font-semibold border-2 relative z-[1]'>
             <div ref={item1Ref} onClick={handleToggle.bind(null, items[0])} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled || activeText}`}>{items[0]}</div>
 
-            <div ref={item2Ref} onClick={handleToggle(items[1])} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled && activeText}`}>{items[1]}</div>
+            <div ref={item2Ref} onClick={()=>{handleToggle(items[1])}} className={`h-8 py-1 px-5 rounded-3xl cursor-pointer ${isToggled && activeText}`}>{items[1]}</div>
 
             <div className='h-8 w-24 rounded-3xl bg-yellow-400 absolute z-[-1] transition-all duration-200 ease-in' style={
                 isToggled ?{
