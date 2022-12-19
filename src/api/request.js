40bf9acb44fa87857e69bbe3247edf1aec1d360e
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-19 11:25:10 +0100

    movies

--- src/api/request.js
+++ src/api/request.js
@@ -1,4 +1,6 @@
 export const request = {
     "On TV": "/tv/popular",
-    "In Theatre": "/movie/popular"
+    "In Theatre": "/movie/popular",
+    "Day": "/trending/movie/day",
+    "Week": "/trending/movie/week"
 }
