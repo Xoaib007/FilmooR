f4245212cd4a2f733a7b19c8b9d7b63cae88c1d9
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-20 19:36:37 +0100

    show

--- src/Pages/SingleMovie/SingleMovie.js
+++ src/Pages/SingleMovie/SingleMovie.js
@@ -6,8 +6,8 @@ const SingleMovie = () => {
 
     return (
         <div className='min-h-screen text-white'>
-            <div className='flex h-[600px] mx-32'>
-                <img className='w-full' src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} alt='' />
+            <div className='flex h-[450px] mx-60'>
+                <img className='w-[300px]' src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${movie.poster_path}`} alt='' />
                 <div className='ml-10'>
                     <p className='text-xl font-bold mb-10'>{movie.title}</p>
                     <p className='text-left'>{movie.overview}</p>
