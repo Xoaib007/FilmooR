39af4697abf0a50dbd54d9e918bd030e02c7871d
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-20 23:02:52 +0100

    show

--- src/Pages/Single/SingleMovie.js
+++ src/Pages/Single/SingleMovie.js
@@ -1,6 +1,6 @@
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
-import { faStar } from '@fortawesome/free-regular-svg-icons'
-import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { faStar, faBookmark } from '@fortawesome/free-regular-svg-icons'
+import { faAngleRight, faAngleDown } from '@fortawesome/free-solid-svg-icons'
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 import Similar from '../../Components/Reusable/Similar';
@@ -18,10 +18,33 @@ const SingleMovie = () => {
             </div>
 
             <div className='flex justify-between mt-10 '>
+
                 <div className='w-1/2 text-left'>
                     <p className=' text-5xl font-bold mb-10'>{movie.title}</p>
                     <p>{movie.overview}</p>
+
+                    <div className='flex'>
+                        <button className='h-12 bg-yellow-400 text-black font-bold rounded-l-xl rounded-r-none mt-10 p-4 hover:bg-yellow-300'>
+                            <div className='flex'>
+                                <FontAwesomeIcon icon={faBookmark} className='w-7 h-7' />
+                                <p className='text-xl'>Add to your watchlist</p>
+                            </div>
+
+                        </button>
+
+                        <div className="dropdown">
+                            <label tabIndex={0} className="btn m-1 rounded-r-xl rounded-l-none hover:bg-yellow-300 bg-yellow-400 text-black font-bold mt-10">
+                                <FontAwesomeIcon icon={faAngleDown} />
+                            </label>
+                            <ul tabIndex={0} className="dropdown-content menu p-2 shadow bg-base-100 rounded-box w-52">
+                                <li><a>Item 1</a></li>
+                                <li><a>Item 2</a></li>
+                            </ul>
+                        </div>
+                    </div>
                 </div>
+
+
                 <div className='mr-32 mt-20'>
                     <div className='flex'>
                         <FontAwesomeIcon className='w-10 h-10' icon={faStar} />
@@ -36,14 +59,14 @@ const SingleMovie = () => {
                 <FontAwesomeIcon className='w-10 h-10' icon={faAngleRight} />
             </div>
             <Credits id={movie.id} />
-            
+
             {/* Similar Section */}
             <div className='flex mb-5 mt-20 border-b-4 border-yellow-400 pb-2'>
                 <p className='text-3xl font-bold'>Similar movies like this</p>
                 <FontAwesomeIcon className='w-10 h-10' icon={faAngleRight} />
             </div>
             <Similar id={movie.id} />
-            
+
             {/* Review Section */}
             <div className='flex mb-5 mt-20 border-b-4 border-yellow-400 pb-2'>
                 <p className='text-3xl font-bold'>User Reviews</p>
@@ -51,7 +74,7 @@ const SingleMovie = () => {
             </div>
             <UsersReview id={movie.id} />
             <button className='bg-yellow-400 text-black font-bold w-1/2 rounded-lg h-10 mt-10'>Add Review</button>
-        </div>
+        </div >
 
 
     );
