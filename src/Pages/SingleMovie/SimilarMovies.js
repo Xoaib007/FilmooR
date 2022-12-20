f08daa1450df6d9dad1db556e8293280bec7d24b
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-20 21:24:17 +0100

    show

--- src/Pages/SingleMovie/SimilarMovies.js
+++ src/Pages/SingleMovie/SimilarMovies.js
@@ -1,16 +1,29 @@
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
+import MovieCard from '../../Components/Reusable/MovieCard';
 
-const SimilarMovies = ({id}) => {
+const SimilarMovies = ({ id }) => {
 
     const { data: similars = [] } = useQuery({
         queryKey: ['similars'],
-        queryFn: () => fetch(`https://api.themoviedb.org/3/movie/${id}/credits?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
+        queryFn: () => fetch(`https://api.themoviedb.org/3/movie/${id}/similar?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
     })
 
+    const slicedSimilars = similars?.results?.slice(0, 14);
+
+    console.log(similars);
+
     return (
-        <div>
+        <div className='flex gap-7 overflow-x-auto pb-10'>
+            {
+                slicedSimilars?.map(similar =>
+                    <MovieCard key={similar.id} {...similar} />
+                )
+            }
             
+            <div className='absolute right-0 w-12 h-[490px] mr-36'>
+                <div className='h-full w-full bg-gradient-to-r from-black/[0%] to-black/[100%]' />
+            </div>
         </div>
     );
 };
