1eb87c372ea0eee69c09d2d0d511edafc80083e3
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-21 23:24:17 +0100

    show

--- src/Components/Reusable/AllCredits.js
+++ src/Components/Reusable/AllCredits.js
@@ -2,8 +2,8 @@ import React from 'react';
 
 const AllCredits = () => {
     return (
-        <div>
-            
+        <div className='min-h-screen text-white'>
+            credits
         </div>
     );
 };
