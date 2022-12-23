d3ab6a5b130d74a7f49fa006650c0a298615ab71
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2022-12-23 21:27:17 +0100

    image null problem solved

--- src/Components/Reusable/ImageCarousel.js
+++ src/Components/Reusable/ImageCarousel.js
@@ -1,6 +1,8 @@
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useQuery } from '@tanstack/react-query';
 import React from 'react';
-import ImageSlider, { Slide } from 'react-auto-image-slider';
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons'
+import { Link } from 'react-router-dom';
 
 const ImageCarousel = ({ type, id }) => {
     console.log(type);
@@ -12,15 +14,35 @@ const ImageCarousel = ({ type, id }) => {
 
     return (
         <div className='bg-gray-900 rounded-lg w-full border-2 ml-5 flex'>
-            <div className="carousel">
-                {
-                    images?.backdrops?.map((image, i) =>
-                        <div id={`${i}`} className="carousel-item relative">
-                            <img src={`https://www.themoviedb.org/t/p/original//${image.file_path}`} className="w-full" alt='' />
-                        </div>
-                    )
-                }
-            </div>
+            {
+                images?.backdrops?.length > 10 ?
+                    <div className="carousel">
+                        {
+                            images?.backdrops?.slice(0, 9).map((image, i) =>
+                                <div id={`${i}`} className="carousel-item relative">
+                                    <img src={`https://www.themoviedb.org/t/p/original//${image.file_path}`} className="w-full" alt='' />
+                                </div>
+                            )
+                        }
+                        
+                        <Link className='carousel-item relative w-full'>
+                            <div className=' mx-auto bg-gray-900 flex mt-48'>
+                                <p className=' text-4xl font-bold '>See All</p>
+                                <FontAwesomeIcon className='w-10 h-10 ' icon={faAngleRight} />
+                            </div>
+                        </Link>
+                    </div>
+                    :
+                    <div className="carousel">
+                        {
+                            images?.backdrops?.map((image, i) =>
+                                <div id={`${i}`} className="carousel-item relative">
+                                    <img src={`https://www.themoviedb.org/t/p/original//${image.file_path}`} className="w-full" alt='' />
+                                </div>
+                            )
+                        }
+                    </div>
+            }
         </div>
     );
 };
