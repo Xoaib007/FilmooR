49b3d9c842b11b889d5283712035df7783f2a492
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-22 15:47:45 +0100

    add

--- src/Pages/EpisodeGuide/EpisodeGuide.js
+++ src/Pages/EpisodeGuide/EpisodeGuide.js
@@ -1,3 +1,5 @@
+import { faStar } from '@fortawesome/free-regular-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
@@ -43,21 +45,31 @@ const EpisodeGuide = () => {
 
                 <div className='mt-20'>
 
-                <div className='text-center mb-10'>
-                    <p className='text-gray-500'>Season: {episodes.season_number}</p>
-                    <p className='text-3xl font-bold'>{episodes.name}</p>
-                </div>
+                    <div className='text-center mb-10'>
+                        <p className='text-gray-500'>Season: {episodes.season_number}</p>
+                        <p className='text-3xl font-bold'>{episodes.name}</p>
+                    </div>
 
                     {
                         episodes?.episodes?.map(episode =>
                             <div key={episode.id} className=''>
-                                <div className='flex mb-5 mx-40'>
+                                <div className='flex mb-5 mx-32'>
+
                                     <img className='w-32 rounded-l-lg' src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${episode.still_path}`} alt='' />
-                                    <div className='text-left p-5 border-y-2 border-r-2 border-gray-400 rounded-r-lg'>
+
+                                    <div className='flex text-left p-5 border-y-2 border-r-2 border-gray-400 rounded-r-lg'>
+                                        <div>
                                         <p className='text-gray-300'>Episode: {episode.episode_number}</p>
                                         <p className='text-2xl font-bold'>{episode.name}</p>
                                         <p>{episode.overview}</p>
+                                        </div>
+
+                                        <div className='flex ml-10'>
+                                            <FontAwesomeIcon className='w-10 h-10' icon={faStar} />
+                                            <p className='text-4xl font-semibold'>{episode.vote_average}<span className='text-2xl font-normal text-gray-400 '>/10 ({episode.vote_count})</span></p>
+                                        </div>
                                     </div>
+
                                 </div>
                             </div>
                         )
