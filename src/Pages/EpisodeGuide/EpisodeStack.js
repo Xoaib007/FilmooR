d86229c4f75b2f7cf379248ce7f19cccb99a4599
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-22 03:54:36 +0100

    add

--- src/Pages/EpisodeGuide/EpisodeStack.js
+++ src/Pages/EpisodeGuide/EpisodeStack.js
@@ -3,7 +3,7 @@ import React from 'react';
 
 const EpisodeStack = ({show,seasonNumber}) => {
 
-    const { data: season = [], refetch } = useQuery({
+    const { data: season = []} = useQuery({
         queryKey: ['images'],
         queryFn: () => fetch(`https://api.themoviedb.org/3/tv/${show}/season/${season}?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
     })
