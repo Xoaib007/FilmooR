b05cab5cd40a3187b829e64a5f7eafa9cc50068f
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-03-22 18:31:46 +0000

    add

--- src/Pages/Single/CastPage/CastPage.js
+++ src/Pages/Single/CastPage/CastPage.js
@@ -4,15 +4,21 @@ import ImageCarousel from '../../../Components/Reusable/ImageCarousel';
 
 const CastPage = () => {
     const person = useLoaderData();
-    
+
     return (
         <div className='min-h-screen text-white mt-10 mx-60'>
             <div className='flex h-[450px]'>
                 <img className='w-[300px]' src={`https://image.tmdb.org/t/p/w600_and_h900_bestv2/${person.profile_path}`} alt='' />
-                <ImageCarousel type='person' id={person.id}/>
+                <ImageCarousel type='person' id={person.id} />
             </div>
 
-            <p className='text-5xl font-bold text-left mt-10'>{person.name}</p>
+            <div>
+                <div className='text-left '>
+                    <p className='text-5xl font-bold mt-10'>{person.name}</p>
+                    <p className='text-xl font-bold mt-5 text-gray-400'>{person.known_for_department}</p>
+                    <p className='text-justify text-gray-400'>{person.biography}</p>
+                </div>
+            </div>
         </div>
     );
 };
