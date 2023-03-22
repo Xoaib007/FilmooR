1b723a1459d98ac4bfa69bcfb398a79b1cddefc3
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-22 15:07:45 +0100

    add

--- src/Pages/EpisodeGuide/EpisodeGuide.js
+++ src/Pages/EpisodeGuide/EpisodeGuide.js
@@ -1,11 +1,20 @@
+import { useQuery } from '@tanstack/react-query';
 import React, { useState } from 'react';
 import { useLoaderData } from 'react-router-dom';
-import EpisodeStack from './EpisodeStack';
 
 const EpisodeGuide = () => {
     const showDetails = useLoaderData();
 
-    const [season, setSeason] = useState(0);
+    const [season, setSeason] = useState(1);
+
+    const { data: episodes = [], isFetching} = useQuery({
+        queryKey: ['images'],
+        queryFn: () => fetch(`https://api.themoviedb.org/3/tv/${showDetails.id}/season/${season}?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
+    })
+
+    if (isFetching) {
+        return <p>Loading</p>
+    }
 
     return (
         <div className='min-h-screen text-white mt-10 mx-60'>
@@ -22,10 +31,11 @@ const EpisodeGuide = () => {
                 <p className='text-xl font-bold border-b-4 border-yellow-300 w-fit p-2 mb-8 mx-auto'>Season</p>
                 {
                     showDetails?.seasons?.map( season =>
-                        <button onClick={()=>setSeason(season.season_number)} key={season.id} className="bg-yellow-300 rounded-full w-10 h-10 mx-5 text-black border-2 border-white">{season.season_number}</button>
+                        <label key={season.id}><button onClick={()=>setSeason(season.season_number)} className="bg-yellow-300 rounded-full w-10 h-10 mx-5 text-black border-2 border-white">{season.season_number}</button></label>
                     )
                 }
-                <EpisodeStack show={showDetails.id} seasonNumber={season}/>
+                {/* <EpisodeStack show={showDetails.id} seasonNumber={season}/> */}
+                <p>{episodes.name}</p>
             </div>
         </div>
     );
