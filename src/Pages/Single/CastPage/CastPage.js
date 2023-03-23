c342b9dc52df2d208606a329e756e28ef8a7cb4b
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-23 03:33:34 +0100

    add

--- src/Pages/Single/CastPage/CastPage.js
+++ src/Pages/Single/CastPage/CastPage.js
@@ -1,8 +1,19 @@
+import { faAngleRight } from '@fortawesome/free-solid-svg-icons';
+import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
+import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
+import MovieCard from '../../../Components/Reusable/MovieCard';
 
 const CastPage = () => {
     const person = useLoaderData();
+
+    const { data: popular = [] } = useQuery({
+        queryKey: ['similars'],
+        queryFn: () => fetch(`https://api.themoviedb.org/3/person/${person.id}/movie_credits?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
+    })
+
+    const sliced = popular?.cast?.slice(0, 14);
 
     return (
         <div className='min-h-screen text-white mt-10 mx-60'>
@@ -15,6 +26,24 @@ const CastPage = () => {
                     <p className='text-gray-400 text-xl'>Born: <span className='text-white'>{person.birthday}</span></p>
                 </div>
             </div>
+
+            <div className='flex mb-5 mt-20 border-b-4 border-yellow-400 pb-2'>
+                <p className='text-3xl font-bold'>Known For</p>
+                <FontAwesomeIcon className='w-10 h-10' icon={faAngleRight} />
+            </div>
+
+            <div className='flex gap-7 overflow-x-auto pb-10'>
+            {
+                sliced?.map(similar =>
+                    <MovieCard key={similar.id} {...similar} />
+                )
+            }
+            
+            <div className='absolute right-0 w-12 h-[490px] mr-60'>
+                <div className='h-full w-full bg-gradient-to-r from-black/[0%] to-black/[100%]' />
+            </div>
+        </div>
+
         </div>
     );
 };
