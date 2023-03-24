42bfabff7bdaa1feac629a9d48373744734c5309
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-24 20:32:26 +0100

    add

--- src/Pages/Authentication/SignIn.js
+++ src/Pages/Authentication/SignIn.js
@@ -31,7 +31,7 @@ const SignIn = () => {
                                 </div>
                                 <button type="submit" class="w-full text-white font-bold bg-yellow-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
-                                    Don’t have an account yet? <Link href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
+                                    Don’t have an account yet? <Link to='/signup' class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
 
                                 </p>
                             </form>
