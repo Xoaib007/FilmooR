0c4045c024d83f9ee6b0a31181c1644db6c25c57
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-24 20:26:11 +0100

    add

--- src/Pages/Authentication/SignIn.js
+++ src/Pages/Authentication/SignIn.js
@@ -3,7 +3,7 @@ import { Link } from 'react-router-dom';
 
 const SignIn = () => {
     return (
-        <div className='min-h-screen text-white mt-10 mx-60'>
+        <div className='min-h-screen text-white mx-60'>
                 <div class="flex flex-col items-center justify-center px-6 py-8 mx-auto md:h-screen lg:py-0">
                     <div class="w-full bg-white rounded-lg shadow dark:border md:mt-0 sm:max-w-md xl:p-0 dark:bg-gray-800 dark:border-gray-700">
                         <div class="p-6 space-y-4 md:space-y-6 sm:p-8">
@@ -28,9 +28,9 @@ const SignIn = () => {
                                             <label for="remember" class="text-gray-500 dark:text-gray-300">Remember me</label>
                                         </div>
                                     </div>
-                                    <Link href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500">Forgot password?</Link>
+                                    <Link href="#" class="text-sm font-medium text-primary-600 hover:underline dark:text-primary-500 text-black">Forgot password?</Link>
                                 </div>
-                                <button type="submit" class="w-full text-white bg-primary-600 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
+                                <button type="submit" class="w-full text-white font-bold bg-yellow-300 hover:bg-primary-700 focus:ring-4 focus:outline-none focus:ring-primary-300 rounded-lg text-sm px-5 py-2.5 text-center dark:bg-primary-600 dark:hover:bg-primary-700 dark:focus:ring-primary-800">Sign in</button>
                                 <p class="text-sm font-light text-gray-500 dark:text-gray-400">
                                     Don’t have an account yet? <Link href="#" class="font-medium text-primary-600 hover:underline dark:text-primary-500">Sign up</Link>
 
