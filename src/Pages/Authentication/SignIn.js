c8faf37d15c5c0b6e3905845a23e6471967bdb67
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-25 04:20:46 +0100

    add

--- src/Pages/Authentication/SignIn.js
+++ src/Pages/Authentication/SignIn.js
@@ -48,7 +48,7 @@ const SignIn = () => {
                                 </div>
                             </div>
                             <div class="flex items-center justify-center mt-8">
-                                <button class="text-white py-2 px-4 uppercase rounded bg-indigo-500 hover:bg-yellow-400 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">Sign In</button>
+                                <button class="text-white py-2 px-4 uppercase rounded bg-yellow-300 hover:bg-yellow-400 shadow hover:shadow-lg font-medium transition transform hover:-translate-y-0.5">Sign In</button>
                             </div>
 
                             <p className='text-gray-400 pt-5'>Not yet registered?<Link to='/signup' className='text-yellow-400 underline hover:text-yellow-300'>Sign up here</Link></p>
