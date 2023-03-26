db85c9f73d8a4d68ff5ee8656752476dd92b0107
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-26 01:49:21 +0100

    add

--- src/Pages/Authentication/SignUp.js
+++ src/Pages/Authentication/SignUp.js
@@ -41,7 +41,7 @@ const SignUp = () => {
                         </div>
                     </div>
                     <div class="relative mt-3">
-                        <input class="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline" id="username" type="text" placeholder="Password" />
+                        <input class="appearance-none border pl-12 border-gray-100 shadow-sm focus:shadow-md focus:placeholder-gray-600  transition  rounded-md w-full py-3 text-gray-600 leading-tight focus:outline-none focus:ring-gray-600 focus:shadow-outline" id="username" type="text" placeholder="Password" required/>
                         <div class="absolute left-0 inset-y-0 flex items-center">
                             <svg xmlns="http://www.w3.org/2000/svg" class="h-7 w-7 ml-3 text-gray-400 p-1" viewBox="0 0 20 20" fill="currentColor"            >
                                 <path d="M10 2a5 5 0 00-5 5v2a2 2 0 00-2 2v5a2 2 0 002 2h10a2 2 0 002-2v-5a2 2 0 00-2-2H7V7a3 3 0 015.905-.75 1 1 0 001.937-.5A5.002 5.002 0 0010 2z" />
