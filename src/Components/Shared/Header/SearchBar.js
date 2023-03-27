71056f24d8996369b7d07e8d3062864b2786601c
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-28 01:57:51 +0200

    add

--- src/Components/Shared/Header/SearchBar.js
+++ src/Components/Shared/Header/SearchBar.js
@@ -2,23 +2,30 @@ import React, { useState } from 'react';
 import { FontAwesomeIcon } from '@fortawesome/react-fontawesome';
 import { faSearch} from '@fortawesome/free-solid-svg-icons'
 import { Link } from 'react-router-dom';
-import { useQuery } from '@tanstack/react-query';
 
-const SearchBar = () => {
-    const [searchInput, setSearchInput] = useState('');
-    console.log(searchInput);
+const SearchBar = ({setResults}) => {
+    const [input, setInput] = useState("");
 
-    const { data: search = [] } = useQuery({
-        queryKey: ['images'],
-        queryFn: () => fetch(`https://api.themoviedb.org/3/search/movie?api_key=6d47a4eb4a550f0aec87d70e03ce12ae&query=${searchInput}&page=1`).then(res => res.json())
-    })
+  const fetchData = (value) => {
+    fetch("https://jsonplaceholder.typicode.com/users")
+      .then((response) => response.json())
+      .then((json) => {
+        const results = json.filter((user) => {
+          return (
+            value &&
+            user &&
+            user.name &&
+            user.name.toLowerCase().includes(value)
+          );
+        });
+        setResults(results);
+      });
+  };
 
-    const handleChange = (value) => {
-        setSearchInput(value);
-        search(value)
-      };
-
-
+  const handleChange = (value) => {
+    setInput(value);
+    fetchData(value);
+  };
     return (
         <div className=" mx-auto hidden lg:flex">
 
@@ -26,7 +33,7 @@ const SearchBar = () => {
 
                 <input onChange={(e)=> handleChange(e.target.value)} type="text" placeholder="Search for a movie, tv show or person..." className="w-96 h-8 rounded-l-full input input-bordered" />
 
-                <Link to={`/search/${searchInput}`} type='submit' className='bg-yellow-400 w-20 rounded-full relative right-10'><FontAwesomeIcon icon={faSearch} /></Link>
+                <Link to={`/search/${input}`} type='submit' className='bg-yellow-400 w-20 rounded-full relative right-10'><FontAwesomeIcon icon={faSearch} /></Link>
 
             </div>
 
