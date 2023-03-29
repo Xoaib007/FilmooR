9928d494832936fe1700474436e2ae7ce794c49f
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-29 13:26:06 +0200

    add

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -58,7 +58,7 @@ const Header = () => {
                         <FontAwesomeIcon className='mt-1' icon={faBookmark} />
                         <p className='ml-3 font-bold'>Watchlist</p>
                     </Link>
-                    <Link to='/signin' className="text-white font-bold hover:text-gray-400 hover:border-2 border-gray-400 px-4 py-2 rounded-xl">Sign In</Link>                               
+                    <Link to='/signin' className="text-white font-bold hover:text-gray-400 hover:border-2 border-gray-400 px-4 py-2 rounded-xl">Sign In</Link>                                
 
                 </div>
             </div>
