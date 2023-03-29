734e7927efcab5f6cba50021af73d9059befda29
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-29 13:20:43 +0200

    add

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -49,6 +49,7 @@ const Header = () => {
                     <SearchBar1 setResults={setResults} />
                     {results && results.length > 0 && <SearchResultsList results={results}/>}
                 </div>
+                
 
 
                 <div className="navbar-end">
