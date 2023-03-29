4288c5e94a3b3968987fc987538ef57612ee5507
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-29 13:21:16 +0200

    add

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -45,6 +45,8 @@ const Header = () => {
                     </Link>
                 </div>
 
+                {/* Searchbar */}
+
                 <div className=' flex-col w-full relative items-stretch'>
                     <SearchBar1 setResults={setResults} />
                     {results && results.length > 0 && <SearchResultsList results={results}/>}
