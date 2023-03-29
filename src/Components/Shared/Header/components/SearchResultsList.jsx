1839a4b54734e6895a57cbcc7d03424feb3ea5b5
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-03-29 16:46:57 +0000

    add

--- src/Components/Shared/Header/components/SearchResultsList.jsx
+++ src/Components/Shared/Header/components/SearchResultsList.jsx
@@ -4,8 +4,8 @@ import { SearchResult } from "./SearchResult";
 export const SearchResultsList = ({ results }) => {
   return (
     <div className="results-list">
-      {results?.results?.map((result, id) => {
-        return <SearchResult result={result.name} key={id} />;
+      {results?.results?.map((result) => {
+        return <SearchResult result={result.name} key={result.id} id={result.id} />;
       })}
     </div>
   );
