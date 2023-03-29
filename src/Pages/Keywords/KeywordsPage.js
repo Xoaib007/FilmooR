2083e1262739c02f6b17e1f1cf4f8e1df3c48886
Shoaib Ullah <55616502+Xoaib007@users.noreply.github.com>
2023-03-29 17:39:43 +0000

    add

--- src/Pages/Keywords/KeywordsPage.js
+++ src/Pages/Keywords/KeywordsPage.js
@@ -1,10 +1,10 @@
 
 import React from 'react';   
 
-export const KeywordPage = () => {
+export const KeywordPage = ( params) => {
   return (
     <div className='min-h-screen text-white mt-10 mx-60'>
-
+        { params}
     </div>
   );
 };
--- src/Routes/Route.js
+++ src/Routes/Route.js
@@ -51,8 +51,7 @@ const router= createBrowserRouter([
         },
         {
             path: '/keyword/:id',
-            element: <KeywordPage/>,
-            loader: ({params}) => fetch(`https://api.themoviedb.org/3/person/${params.id}?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`)
+            element: <KeywordPage/>
         },
         {
             path: '/signin',
