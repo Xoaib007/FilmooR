026b92af058857cb8bbedba7ff85feeb59aa593a
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-29 13:22:46 +0200

    add

--- src/Components/Shared/Header/Header.js
+++ src/Components/Shared/Header/Header.js
@@ -50,8 +50,7 @@ const Header = () => {
                 <div className=' flex-col w-full relative items-stretch'>
                     <SearchBar1 setResults={setResults} />
                     {results && results.length > 0 && <SearchResultsList results={results}/>}
-                </div>
-                
+                </div>         
 
                 <div className="navbar-end">
 
