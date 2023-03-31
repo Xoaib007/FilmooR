c7b429bad8773a3e1a17d27eff798e206731a7d6
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-31 13:16:28 +0200

    adds

--- src/Pages/Keywords/KeywordsPage.js
+++ src/Pages/Keywords/KeywordsPage.js
@@ -1,12 +1,17 @@
-import React from 'react';   
+import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-export const KeywordPage = ({query}) => {
+export const KeywordPage = ({ query }) => {
   const keyword = useLoaderData();
+
+  const { data: popular = [] } = useQuery({
+    queryKey: ['similars'],
+    queryFn: () => fetch(`https://api.themoviedb.org/3/person/${person.id}/movie_credits?api_key=6d47a4eb4a550f0aec87d70e03ce12ae`).then(res => res.json())
+  })
 
   return (
     <div className='min-h-screen text-white mt-10 mx-60'>
-        { query}
+      {query}
     </div>
   );
 };
