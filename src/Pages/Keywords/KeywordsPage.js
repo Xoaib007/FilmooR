85bd76b00060a1914937d3e9265518466b370f52
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-31 13:15:21 +0200

    adds

--- src/Pages/Keywords/KeywordsPage.js
+++ src/Pages/Keywords/KeywordsPage.js
@@ -2,6 +2,8 @@
 import React from 'react';   
 
 export const KeywordPage = ({query}) => {
+  const keyword = useLoaderData();
+
   return (
     <div className='min-h-screen text-white mt-10 mx-60'>
         { query}
