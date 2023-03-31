c2127a27d66fdf66154f972293cb736e01fae58f
Shoaib Ullah <Shoaib007Rehman@gmail.com>
2023-03-31 13:22:09 +0200

    adds

--- src/Pages/Keywords/KeywordsPage.js
+++ src/Pages/Keywords/KeywordsPage.js
@@ -2,7 +2,7 @@ import { useQuery } from '@tanstack/react-query';
 import React from 'react';
 import { useLoaderData } from 'react-router-dom';
 
-export const KeywordPage = ({ query }) => {
+export const KeywordPage = () => {
   const keyword = useLoaderData();
 
   const { data: movies = [] } = useQuery({
@@ -20,9 +20,10 @@ export const KeywordPage = ({ query }) => {
     queryFn: () => fetch(`https://api.themoviedb.org/3/search/person?api_key=6d47a4eb4a550f0aec87d70e03ce12ae&query=${keyword.name}`).then(res => res.json())
   })
 
+  console.log(movies, tv, person);
+
   return (
     <div className='min-h-screen text-white mt-10 mx-60'>
-      {query}
     </div>
   );
 };
